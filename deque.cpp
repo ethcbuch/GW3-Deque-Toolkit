84ee6d8dcff391581a95666b15b88c0ff2a2bfc4
Hunter Johnson <hunmjohn@ut.utm.edu>
2022-12-01 14:55:03 -0600

    it works

--- deque.cpp
+++ deque.cpp
@@ -2,38 +2,57 @@
 
 using namespace std;
 
+int deque::getColumns()
+{
+  return columns;
+}
 
 
 void deque::Allocation(string fileName)
 {
   ifstream inputFile;
+  int **temp;
+  int temp2;
   inputFile.open(fileName);
   columns = 1;
-  blockmap = new *int[columns];
+  blockmap = new int*[columns];
   for(int i = 0; i < columns; i++)
     {
       blockmap[i] = new int[bsize];
       for(int v = 0; v < bsize; v++)
 	{
-	  if(inputFile)
+	  temp2 = inputFile.peek();
+	  if(temp2 != -1)
 	    {
 	      inputFile >> blockmap[i][v];
+	      inputFile.get();
+	      size++;
 	    }
 	}
       if(inputFile)
 	{
 	  columns++;
-	  int **temp;
-	  temp = new *int[columns];
+	  temp = new int*[columns];
 	  for(int f = 0; f < (columns - 1); f++)
 	    {
 	      temp[f] = blockmap[f];
 	    }
 	  blockmap = temp;
-	  delete[] temp;
+	}
+      if(temp2 == -1)
+	{
+	  columns--;
 	}
     }
+  temp = new int*[columns];
+  delete[] temp;
   inputFile.close();
+  /* size = size - 1;
+  if(size%bsize == 0)
+    {
+      columns - 1;
+    }
+  */
 }
 
 
@@ -48,4 +67,5 @@ void deque::print()
 	}
       cout << "end" << endl;
     }
+  cout << endl << size << endl;
 }
